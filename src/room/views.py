cd5b8d758480ba4d173f88aa5335609900780a6e
Huerte <huertejerald@gmail.com>
2025-09-02 21:24:21 +0800

    add: pages added

--- src/room/views.py
+++ src/room/views.py
@@ -8,9 +8,21 @@ def all_room(request):
 def room_view(request):
     return render(request, 'room.html')
 
+def activity_view(request):
+    return render(request, 'activity_teacher.html')
+
+
 ## Student Edition
 def all_room_s(request):
     return render(request, 'rooms-student.html')
 
 def room_view_s(request):
     return render(request, 'room-student.html')
+
+def activity_view_s(request):
+    return render(request, 'activity_student.html')
+
+
+
+def announcement_view(request):
+    return render(request, 'announcement.html')

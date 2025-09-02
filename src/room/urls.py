0d587fa7f2d3795678295e87f99c3450892da653
Huerte <huertejerald@gmail.com>
2025-09-02 18:45:18 +0800

    refactor: room separated between roles

--- src/room/urls.py
+++ src/room/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.room_view, name='room'),
-    path('all/', views.all_room, name='all_room')
+    path('all/', views.all_room, name='all_room'),
+
+    path('student-edition/', views.room_view_s, name='room_s'),
+    path('all/student-edition', views.all_room_s, name='all_room_s'),
 ]
--- src/room/views.py
+++ src/room/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 
+## Teacher Edition
 def all_room(request):
     return render(request, 'rooms.html')
 
 def room_view(request):
     return render(request, 'room.html')
 
+## Student Edition
+def all_room_s(request):
+    return render(request, 'rooms-student.html')
+
+def room_view_s(request):
+    return render(request, 'room-student.html')

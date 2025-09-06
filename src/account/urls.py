84d1334b0dcf41a3147ba5e5ce9ee91c18b6fb3a
Huerte <huertejerald@gmail.com>
2025-09-06 13:06:44 +0800

    enhance: pages design improve

--- src/account/urls.py
+++ src/account/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('register/', views.register_page, name='register_page'),
     path('login/user/', views.login_user, name='login_user'),
     path('register/user/', views.register_user, name='register_user'),
+    path('profile/teacher/', views.profile_teacher, name='profile_teacher'),
+    path('profile/student/', views.profile_student, name='profile_student'),
+    path('view/teacher/<int:teacher_id>/', views.view_teacher_profile, name='view_teacher_profile'),
+    path('view/student/<int:student_id>/', views.view_student_profile, name='view_student_profile'),
 ]

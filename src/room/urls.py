f9db3956f4d9f5a418e5e4f7375992b7e137369c
Huerte <huertejerald@gmail.com>
2025-09-23 18:38:49 +0800

    enhance: dashboard for both student and teacher improve

--- src/room/urls.py
+++ src/room/urls.py
@@ -9,12 +9,14 @@ urlpatterns = [
     path('announcement/<int:announcement_id>/', views.announcement_view, name='announcement'),
 
     path('create/', views.create_room, name='create_room'),
+    path('delete/<int:announcement_id>/', views.delete_room, name='delete_room'),
     path('enroll/', views.enroll_student, name='enroll_student'),
     
     path('activity/create/', views.create_activity, name='create_activity'),
     path('announcement/create/', views.create_announcement, name='create_announcement'),
 
     path('activity/submit/', views.submit_activity, name='submit_activity'),
+    path('activity/grade/', views.grade_submission, name='grade_submission'),
 
     path('<str:room_id>/', views.room_view, name='room'),
 ]

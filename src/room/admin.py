6ea847f7a01d2e81b64523cf7d6599bd471c9f54
Huerte <huertejerald@gmail.com>
2025-09-26 18:38:51 +0800

    add: submission model is included in admin page

--- src/room/admin.py
+++ src/room/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Room, Activity, Announcement
+from .models import Room, Activity, Announcement, Submission
 
 
 admin.site.register(Room)
 admin.site.register(Activity)
 admin.site.register(Announcement)
+admin.site.register(Submission)

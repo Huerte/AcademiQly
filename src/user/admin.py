5838af4602053ddbe4421876445d2b70eecbd0d6
Huerte <huertejerald@gmail.com>
2025-09-27 08:18:27 +0800

    feature: made the user profile field dynamic

--- src/user/admin.py
+++ src/user/admin.py
@@ -3,3 +3,6 @@ from .models import *
 
 admin.site.register(StudentProfile)
 admin.site.register(TeacherProfile)
+admin.site.register(Course)
+admin.site.register(YearLevel)
+admin.site.register(Qualification)
